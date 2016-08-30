bbebbacfca48f74c7056975d2e4221eac346f0ab
Thinkman Wang <wangxf1985@gmail.com>
2016-08-30 19:23:01 +0300

    update

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 #coding=utf-8
 
 import sys
-reload(sys) # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入 
-sys.setdefaultencoding('utf-8')
+from imp import reload
+
+if sys.version[0] == '2':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
 
 import os
 
@@ -25,11 +28,11 @@ app = Flask(__name__, static_url_path = "/upload", static_folder = "upload")
 app.config['UPLOAD_FOLDER'] = 'upload/'
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'TXT', 'PDF', 'PNG', 'JPG', 'JPEG', 'GIF'])
 
-dict_err_code = { 
-    0 : "success" 
-    , 1 : "Server support post only" 
-    , 10 : "Upload failed" 
-    , 11 : "No file" 
+dict_err_code = {
+    0 : "success"
+    , 1 : "Server support post only"
+    , 10 : "Upload failed"
+    , 11 : "No file"
     , 1000 : "Unknow error"
 }
 
@@ -42,7 +45,7 @@ def get_file_ext(filename):
         return filename.rsplit('.', 1)[1]
     else:
         return ""
-    
+
 @app.route("/", methods=['POST', 'GET'])
 def index():
     return "Server API for Notes!"
@@ -51,13 +54,13 @@ def index():
 def upload_file():
     if request.method == 'GET':
         return obj2json(RetModel(1, dict_err_code[1], {}) )
-      
-    
+
+
     dictFiles = {};
-    
+
     if 'pic1' not in request.files:
-        return obj2json(RetModel(11, dict_err_code[11], {}) ) 
-    
+        return obj2json(RetModel(11, dict_err_code[11], {}) )
+
     file1 = request.files['pic1']
     if file1 and allowed_file(file1.filename):
         filename = secure_filename(file1.filename)
@@ -65,107 +68,107 @@ def upload_file():
         savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
         file1.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
         dictFiles["pic1"] = savedFileName;
-        
+
     else:
-        return obj2json(RetModel(10, dict_err_code[10], {}) ) 
-    
+        return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic2' in request.files:
         file2= request.files['pic2']
         if file2 and allowed_file(file2.filename):
             filename = secure_filename(file2.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file2.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic2"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )     
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic3' in request.files:
         file3= request.files['pic3']
         if file3 and allowed_file(file3.filename):
             filename = secure_filename(file3.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file3.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic3"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )       
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic4' in request.files:
         file4= request.files['pic4']
         if file4 and allowed_file(file4.filename):
             filename = secure_filename(file4.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file4.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic4"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )         
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic5' in request.files:
         file5= request.files['pic5']
         if file5 and allowed_file(file5.filename):
             filename = secure_filename(file5.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file5.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic5"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )      
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic6' in request.files:
         file6= request.files['pic6']
         if file6 and allowed_file(file6.filename):
             filename = secure_filename(file6.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file6.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic6"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )      
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic7' in request.files:
         file7= request.files['pic7']
         if file7 and allowed_file(file7.filename):
             filename = secure_filename(file7.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file7.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic7"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )    
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic8' in request.files:
         file8= request.files['pic8']
         if file8 and allowed_file(file8.filename):
             filename = secure_filename(file8.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file8.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic8"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )        
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     if 'pic9' in request.files:
         file9= request.files['pic9']
         if file9 and allowed_file(file9.filename):
             filename = secure_filename(file9.filename)
-    
+
             savedFileName = str(uuid.uuid4()) + "." + get_file_ext(filename)
             file9.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
             dictFiles["pic9"] = savedFileName;
-            
+
         else:
-            return obj2json(RetModel(10, dict_err_code[10], {}) )      
-    
+            return obj2json(RetModel(10, dict_err_code[10], {}) )
+
     return obj2json(RetModel(0, dict_err_code[0], dictFiles) )
-    
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
